e9158f067c5ad5856baee6454d20d9e8bcc70ff3
Samrat-14 <samratdewdrops0@gmail.com>
2024-11-09 10:23:54 +0530

    fix: change db from sqlite to postgresql for prod

--- app/workflow/_components/nodes/node-component.tsx
+++ app/workflow/_components/nodes/node-component.tsx
@@ -5,12 +5,9 @@ import NodeCard from '@/app/workflow/_components/nodes/node-card';
 import NodeHeader from '@/app/workflow/_components/nodes/node-header';
 import { NodeInput, NodeInputs } from '@/app/workflow/_components/nodes/node-inputs';
 import { NodeOutput, NodeOutputs } from '@/app/workflow/_components/nodes/node-outputs';
-import { Badge } from '@/components/ui/badge';
 
 import { TaskRegistry } from '@/lib/workflow/task/registry';
 import { AppNodeData } from '@/types/appnode';
-
-const DEV_MODE = process.env.NEXT_PUBLIC_DEV_MODE === 'true';
 
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
@@ -18,7 +15,6 @@ const NodeComponent = memo((props: NodeProps) => {
 
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
-      {DEV_MODE && <Badge>DEV: {props.id}</Badge>}
       <NodeHeader taskType={nodeData.type} nodeId={props.id} />
       <NodeInputs>
         {task.inputs.map((input) => (
--- prisma/migrations/20241106114547_added_user_balance_model/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "UserBalanace" (
-    "userId" TEXT NOT NULL PRIMARY KEY,
-    "credits" INTEGER NOT NULL DEFAULT 0
-);
